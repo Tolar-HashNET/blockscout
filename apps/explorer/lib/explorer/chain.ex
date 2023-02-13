092dff9a67a22781ef75b1b8c15a9a75bfd15bc2
Alex Maslo <maslo.rails@gmail.com>
2023-02-13 16:37:58 +0200

    added eth_address_to_tolar/1 function and use it to transform transaction addresses

--- apps/explorer/lib/explorer/chain.ex
+++ apps/explorer/lib/explorer/chain.ex
@@ -2130,7 +2130,7 @@ defmodule Explorer.Chain do
   def fetch_transaction_by_hash(transaction_hash) do
     Transaction
     |> where([t], t.hash == ^transaction_hash)
-    |> preload([:block])
+    |> preload([:block, :from_address, :to_address])
     |> Repo.one()
   end
 
