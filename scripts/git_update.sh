754be3a1065be01561f6fa4545177f6f4a5f1c0e
MarioMatosevic <109737434+MarioMatosevic@users.noreply.github.com>
2023-02-22 15:29:03 +0100

    Update git_update.sh

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -28,7 +28,7 @@ VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-if [[ $VERSION == 'major' ]]
+if [[ $VERSION == 'tolar_master' ]]
 then
   VNUM1=v$((VNUM1+1))
 elif [[ $VERSION == 'minor' ]]
@@ -38,7 +38,7 @@ elif [[ $VERSION == 'patch' ]]
 then
   VNUM3=$((VNUM3+1))
 else
-  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [tolar_master, minor, patch]"
   exit 1
 fi
 
