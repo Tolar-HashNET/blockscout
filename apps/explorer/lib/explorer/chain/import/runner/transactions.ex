d431b0695e7921c0d008b45151495c7c547277b4
Alex Maslo <maslo.rails@gmail.com>
2023-03-19 16:24:45 +0200

    broken: added logic to fetch and store additional tolar fields

--- apps/explorer/lib/explorer/chain/import/runner/transactions.ex
+++ apps/explorer/lib/explorer/chain/import/runner/transactions.ex
@@ -11,6 +11,8 @@ defmodule Explorer.Chain.Import.Runner.Transactions do
   alias Explorer.Chain.{Block, Hash, Import, Transaction}
   alias Explorer.Chain.Import.Runner.TokenTransfers
 
+  alias Explorer.Chain.Transaction.TolarTransactionData
+
   @behaviour Import.Runner
 
   # milliseconds
@@ -50,6 +52,9 @@ defmodule Explorer.Chain.Import.Runner.Transactions do
     |> Multi.run(:transactions, fn repo, _ ->
       insert(repo, changes_list, insert_options)
     end)
+    |> Multi.run(:tolar_transaction_data, fn repo, _ ->
+      derive_tolar_transaction_data(repo, changes_list, insert_options)
+    end)
   end
 
   @impl Import.Runner
@@ -79,7 +84,11 @@ defmodule Explorer.Chain.Import.Runner.Transactions do
     on_conflict = Map.get_lazy(options, :on_conflict, &default_on_conflict/0)
 
     # Enforce Transaction ShareLocks order (see docs: sharelocks.md)
-    ordered_changes_list = Enum.sort_by(changes_list, & &1.hash)
+    # We need to drop tolar specific fields here to allow for `insert_all`
+    ordered_changes_list =
+      changes_list
+      |> Enum.map(&Map.drop(&1, [:gas_refunded, :network_id, :output]))
+      |> Enum.sort_by(& &1.hash)
 
     Import.insert_changes_list(
       repo,
@@ -87,6 +96,31 @@ defmodule Explorer.Chain.Import.Runner.Transactions do
       conflict_target: :hash,
       on_conflict: on_conflict,
       for: Transaction,
+      returning: true,
+      timeout: timeout,
+      timestamps: timestamps
+    )
+  end
+
+  defp derive_tolar_transaction_data(
+         repo,
+         changes_list,
+         %{
+           timeout: timeout,
+           timestamps: timestamps
+         }
+       ) do
+    # Enforce Transaction ShareLocks order (see docs: sharelocks.md)
+    ordered_tolar_transaction_changes_list =
+      changes_list
+      |> Enum.map(&Map.take(&1, TolarTransactionData.allowed_attrs()))
+      |> Enum.map(&parse_output/1)
+      |> Enum.sort_by(& &1.hash)
+
+    Import.insert_changes_list(
+      repo,
+      ordered_tolar_transaction_changes_list,
+      for: TolarTransactionData,
       returning: true,
       timeout: timeout,
       timestamps: timestamps
@@ -217,6 +251,20 @@ defmodule Explorer.Chain.Import.Runner.Transactions do
         postgrex_error in Postgrex.Error ->
           {:error, %{exception: postgrex_error, block_hashes: block_hashes}}
       end
+    end
+  end
+
+  defp parse_output(attrs) when not is_map_key(attrs, :output), do: attrs
+
+  defp parse_output(%{output: nil} = attrs), do: attrs
+
+  defp parse_output(%{output: output} = attrs) do
+    case Explorer.Chain.Data.cast(output) do
+      {:ok, data} ->
+        Map.put(attrs, :output, data)
+
+      _ ->
+        attrs
     end
   end
 end
--- apps/explorer/priv/repo/migrations/20230319110706_create_tolar_transaction_data.exs
+++ apps/explorer/priv/repo/migrations/20230319110706_create_tolar_transaction_data.exs
@@ -5,12 +5,12 @@ defmodule Explorer.Repo.Migrations.CreateTolarTransactionData do
     create table(:tolar_transaction_data, primary_key: false) do
       add(:hash, references(:transactions, column: :hash, on_delete: :delete_all, type: :bytea), null: false)
 
-      add(:gas_used, :numeric, precision: 100, null: true)
+      add(:gas_refunded, :numeric, precision: 100, null: true)
 
       add(:network_id, :integer, null: false)
       add(:output, :bytea, null: false)
 
-      timestamps()
+      timestamps(null: false, type: :utc_datetime_usec)
     end
   end
 end
